959d15b296f06dd10fbfbdc5289937d599b1b830
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2024-04-16 16:55:56 +0200

    Add additional parameters

--- benches/atact.rs
+++ benches/atact.rs
@@ -20,78 +20,80 @@ fn bench_atact(
     let (pp, issuers) = setup(num_issuers, n, t, tprime, &attributes);
 
     c.bench_function(
-        &format!("token_request {:?}", (num_issuers, n, t, tprime)),
+        &format!(
+            "token_request (N, n, t, t') = {:?}",
+            (num_issuers, n, t, tprime)
+        ),
         |b| {
             b.iter(|| {
-                let (blind_request, rand) = token_request(a, &pp).expect("token request failed");
-                black_box(blind_request);
-                black_box(rand);
+                black_box(token_request(a, &pp).expect("token request failed"));
             })
         },
     );
 
     let (blind_request, rand) = token_request(a, &pp).expect("token request failed");
-    c.bench_function(&format!("tissue {:?}", (num_issuers, n, t, tprime)), |b| {
-        b.iter(|| {
-            let blind_token = tissue(&blind_request, &issuers[0], &pp).expect("tissue failed");
-            black_box(blind_token);
-        });
-    });
-
-    let mut blind_tokens = vec![];
-    for issuer in &issuers {
-        let blind_token = tissue(&blind_request, issuer, &pp).expect("tissue failed");
-        blind_tokens.push(blind_token);
-    }
-
     c.bench_function(
-        &format!("aggregate/unblind {:?}", (num_issuers, n, t, tprime)),
+        &format!("tissue (N, n, t, t') = {:?}", (num_issuers, n, t, tprime)),
         |b| {
             b.iter(|| {
-                let token = aggregate_unblind(&blind_tokens, &rand, &pp);
-                black_box(token);
+                black_box(tissue(&blind_request, &issuers[0], &pp).expect("tissue failed"));
+            });
+        },
+    );
+
+    let blind_tokens = issuers
+        .iter()
+        .map(|issuer| tissue(&blind_request, issuer, &pp).expect("tissue failed"))
+        .collect();
+    c.bench_function(
+        &format!(
+            "aggregate/unblind (N, n, t, t') = {:?}",
+            (num_issuers, n, t, tprime)
+        ),
+        |b| {
+            b.iter(|| {
+                black_box(aggregate_unblind(&blind_tokens, &rand, &pp));
             });
         },
     );
 
     let token = aggregate_unblind(&blind_tokens, &rand, &pp);
-    c.bench_function(&format!("proof {:?}", (num_issuers, n, t, tprime)), |b| {
-        b.iter(|| {
-            let token_proof = prove(&token, &rand, &pp);
-            black_box(token_proof);
-        });
-    });
+    c.bench_function(
+        &format!("proof (N, n, t, t') = {:?}", (num_issuers, n, t, tprime)),
+        |b| {
+            b.iter(|| {
+                black_box(prove(&token, &rand, &pp));
+            });
+        },
+    );
 
     let token_proof = prove(&token, &rand, &pp);
-    c.bench_function(&format!("verify {:?}", (num_issuers, n, t, tprime)), |b| {
-        b.iter(|| {
-            #[allow(unused_must_use)]
-            {
-                black_box(verify(&token, &token_proof, &blind_request, &pp));
-            }
-        });
-    });
+    c.bench_function(
+        &format!("verify (N, n, t, t') = {:?}", (num_issuers, n, t, tprime)),
+        |b| {
+            b.iter(|| {
+                #[allow(unused_must_use)]
+                {
+                    black_box(verify(&token, &token_proof, &blind_request, &pp));
+                }
+            });
+        },
+    );
 }
 
-fn bench_params_1(c: &mut Criterion) {
-    const NUM_ISSUERS: usize = 50;
-    const N: usize = 40;
-    const T: usize = NUM_ISSUERS / 2 + 1;
-    const TPRIME: usize = N / 2 + 1;
-    const NUM_ATTRIBUTES: usize = 10;
+const NUM_ISSUERS: [usize; 3] = [4, 16, 64];
+const N: [usize; 3] = [30, 40, 128];
+const NUM_ATTRIBUTES: usize = 10;
 
-    bench_atact(c, NUM_ATTRIBUTES, NUM_ISSUERS, N, T, TPRIME);
+fn bench_params(c: &mut Criterion) {
+    for num_issuers in NUM_ISSUERS {
+        for n in N {
+            let t = num_issuers / 2 + 1;
+            let tprime = n / 2 + 1;
+            bench_atact(c, NUM_ATTRIBUTES, num_issuers, n, t, tprime)
+        }
+    }
 }
 
-fn bench_params_2(c: &mut Criterion) {
-    const NUM_ISSUERS: usize = 50;
-    const N: usize = 128;
-    const T: usize = NUM_ISSUERS / 2 + 1;
-    const TPRIME: usize = N / 2 + 1;
-    const NUM_ATTRIBUTES: usize = 10;
-
-    bench_atact(c, NUM_ATTRIBUTES, NUM_ISSUERS, N, T, TPRIME);
-}
-
-criterion_group!(benches, bench_params_1, bench_params_2);
+criterion_group!(benches, bench_params);
 criterion_main!(benches);
